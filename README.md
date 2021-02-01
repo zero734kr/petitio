9b73a3ed28bda9254a18a0717ad33fac1a91f41d
tbnritzdoge <shadowphoenixcoc@gmail.com>
2021-02-01 16:59:35 -0500

    feat: add docs

--- README.md
+++
@@ -1,3 +0,0 @@
-# @helperdiscord/centra
-
-A cool library to use for rest requests in nodejs :)
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-import CentraRequest, { HTTPMethod } from './lib/CentraRequest';
-
-export = (url: URL | string, method: HTTPMethod = 'GET') => new CentraRequest(url, method);
